f171b93062a7d802bfed1e5c05842f44f4643526
Henriqueheruster <henriquezxt@gmail.com>
2023-12-28 10:45:38 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// RELAÇÕES
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?~@#$%&*.^"
+let novaSenha = "";
+
+// ALGORITMO
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+
+    for(let i=0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+        
+    }
+
+    containerPassword.classList.remove("hide")
+    password.innerHTML = pass;
+    novaSenha = pass;
+
+}
+
+function copyPassword(){
+    navigator.clipboard.writeText(novaSenha)
+    .then(() => {
+        console.log('Senha copiada com sucesso!');
+    })
+    .catch(err => {
+        console.error('Erro ao copiar senha: ', err);
+    });
+}
+
